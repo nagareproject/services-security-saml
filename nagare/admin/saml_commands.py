8029dcfc974316272075ed5f74f50dab939da29d
Alain Poirier <alain.poirier@net-ng.com>
2021-07-21 16:48:18 +0200

    SAML service search added

--- nagare/admin/saml_commands.py
+++ nagare/admin/saml_commands.py
@@ -18,23 +18,57 @@ class Commands(command.Commands):
 class Metadata(command.Command):
     DESC = 'display SP metadata'
 
+    def set_arguments(self, parser):
+        super(Metadata, self).set_arguments(parser)
+        parser.add_argument('-n', '--name', help='name of the SAML service name')
+
     @staticmethod
-    def run(services_service):
+    def run(name, services_service):
         status = 0
 
-        for saml_service in services_service.values():
-            if not isinstance(saml_service, saml_auth.Authentication):
-                continue
+        if name:
+            saml_service_name = name.strip('/').split('/')
+        else:
 
-            errors, metadata = saml_service.get_sp_metadata()
+            def to_names(services):
+                for service, children in services:
+                    name = (service.name,)
 
-            if errors:
-                print('Validation errors: ' + ', '.join(errors))
-                status = 1
-            else:
-                if isinstance(metadata, bytes):
-                    metadata = metadata.decode('utf-8')
+                    if not children:
+                        yield name
+                    else:
+                        for e in to_names(children):
+                            yield name + tuple(e)
 
-                print(metadata)
+            saml_services = services_service.find_services(lambda service: isinstance(service, saml_auth.Authentication))
+            names = list(to_names(saml_services))
+            if not names:
+                print('No SAML service found')
+                return 1
+
+            if len(names) > 1:
+                names = ['/'.join(name) for name in names]
+                print('Several SAML service found:', ', '.join(names))
+                print('Select one with the `-n` option')
+                return 1
+
+            saml_service_name = names[0]
+
+        try:
+            saml_service = services_service.get_service(saml_service_name)
+        except KeyError:
+            print('SAML service {} not found'.format(repr(name)))
+            return 1
+
+        errors, metadata = saml_service.get_sp_metadata()
+
+        if errors:
+            print('Validation errors: ' + ', '.join(errors))
+            status = 1
+        else:
+            if isinstance(metadata, bytes):
+                metadata = metadata.decode('utf-8')
+
+            print(metadata)
 
         return status
